cc42cd4d2c786db954a637e68cd1439896af9180
Kevin Oliveira <kevinols08@gmail.com>
2019-10-31 11:07:07 -0300

    Alteração da OID

--- snmp.php
+++ snmp.php
@@ -4,14 +4,15 @@ $session = new SNMP(SNMP::VERSION_2c, "201.30.47.4", "public");
 $ifSpeed = $session->get("1.3.6.1.2.1.2.2.1.5.1");
 
 $delta_in = $session->get("1.3.6.1.2.1.2.2.1.10.1");
-sleep(1);
-$delta_in = $session->get("1.3.6.1.2.1.2.2.1.10.1") - $delta_in;
-$in_bw = $delta_in * 8 * 100/$ifSpeed;
+$delta_out = $session->get("1.3.6.1.2.1.2.2.1.16.1");
 
-$delta_out = $session->get("1.3.6.1.2.1.2.2.1.10.1");
 sleep(1);
-$delta_out = $session->get("1.3.6.1.2.1.2.2.1.10.1") - $delta_out;
-$out_bw = $delta_out * 8 * 100/$ifSpeed;
+
+$delta_in = $session->get("1.3.6.1.2.1.2.2.1.10.1") - $delta_in;
+$delta_out = $session->get("1.3.6.1.2.1.2.2.1.16.1") - $delta_out;
+
+$in_bw = round($delta_in * 8 * 100/$ifSpeed,2);
+$out_bw = round($delta_out * 8 * 100/$ifSpeed,2);
 
 echo json_encode(array("down" => $in_bw,"up" => $out_bw));
 ?>
