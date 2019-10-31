a68dd8580d1b9f319d0aa824a7e50f3d6a24cfea
kevnoli <kevinols08@gmail.com>
2019-10-31 14:14:36 -0300

    Múltiplas séries de dados

--- snmp.js
+++ snmp.js
@@ -1,37 +1,39 @@
 window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer", {
-        data: [
-        {
-            type: "spline",
-            dataPoints: [
-            { y: 0}
-            ],
-            dataPoints: [
-            { y: 0}
-            ]
-        }
-        ],
-         axisX:{
-   minimum: 0,
-   interval: 1,
-   intervalType: "second",
- },
-    });
-    chart.render(); 
+
+    var download = [{y: 0}];
+    var upload = [{y: 0}];
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      data: [
+      { 
+        type: "line",
+        showInLegend: true,
+        name: "Download",
+        dataPoints: download
+    },
+    {               
+        type: "line",
+        showInLegend: true,
+        name: "Upload" ,
+        dataPoints: upload
+    }],
+    axisX:{
+     minimum: 0,
+     interval: 1,
+     intervalType: "second"
+ }
+});
+
+    chart.render();
 
     $("#iniciarBtn").click(function () {
-
-    var length = chart.options.data[0].dataPoints.length;
-    chart.options.data[0].dataPoints.push({ y: 25 - Math.random() * 10});
-    chart.render();
+        download.push({ y: 25 - Math.random() * 10});
+        upload.push({ y: 10 - Math.random() * 10});
+        chart.render();
 
     });
 
     $("#pararBtn").click(function () {
-
-    var length = chart.options.data[0].dataPoints.length;
-    chart.options.data[0].dataPoints[length-1].y = 15 - Math.random() * 10;
-    chart.render();
-
+//TODO
     });
 }
